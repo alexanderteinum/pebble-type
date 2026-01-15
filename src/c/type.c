f84005d0a2c0f25cd59e8a22f2f9867591331f12
Morten Teinum <morten.teinum@telia.no>
2026-01-16 00:18:32 +0100

    Re-run clang-format to match CI expectations

--- src/c/type.c
+++ src/c/type.c
@@ -88,9 +88,8 @@ static void init()
 {
   s_main_window = window_create();
 
-  window_set_window_handlers(s_main_window,
-                              (WindowHandlers){.load   = main_window_load,
-                                               .unload = main_window_unload});
+  window_set_window_handlers(
+      s_main_window, (WindowHandlers) {.load = main_window_load, .unload = main_window_unload});
 
   window_stack_push(s_main_window, true);
 
